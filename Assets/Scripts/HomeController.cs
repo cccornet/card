effb5387933adeba92f6632147212ef5a6200167
t-ryo <t.ryosuke.fv@gmail.com>
2018-12-19 23:12:25 +0900

    implemented Title->Home

---
+++ Assets/Scripts/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class HomeController : MonoBehaviour {
+
+	//void Start () {
+		
+	//}
+
+	//void Update () {
+		
+	//}
+}
---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour {
+
+	//void Start () {
+		
+	//}
+	
+	void Update () {
+        TouchInfo info = AppUtil.GetTouch();
+        if (info == TouchInfo.Began) {
+            Debug.Log("Touch !");
+            SceneManager.LoadScene("Home");
+        }
+	}
+}
